19b8f21393c6d680369eb1d0f094a287180c5846
Jim McStanton <Jim.McStanton@garmin.com>
2022-06-12 18:47:53 -0500

    working on resources

--- src/components/Resources/Resources.tsx
+++ src/components/Resources/Resources.tsx
@@ -5,7 +5,9 @@ import {
   AccordionSummary,
   Typography,
 } from "@mui/material";
+import { parse }    from 'yaml';
 import ResourceCard from "../ResourceCard/ResourceCard";
+import rawDetails   from "./resourceDetails";
 
 const accordionProps = {
   variant: "outlined" as "outlined",
@@ -31,115 +33,26 @@ const HorizontalHeaderLine = () => (
   />
 );
 
+const resourceDetails : any = parse(rawDetails);
+
 const Resources = () => {
   return (
-    <div>
-      <Typography variant="h4" style={{ marginBottom: 25 }}>
-        Resources
-      </Typography>
-      <Accordion {...accordionProps}>
-        <AccordionSummary expandIcon={<ExpandMore />}>
-          <Typography {...accordionHeaderProps}>Housing</Typography>
-        </AccordionSummary>
-        <HorizontalHeaderLine />
-        <AccordionDetails>
-          <ResourceCard
-            title="Restart Inc"
-            website="https://www.restartinc.org/"
-            hours="Open 24 hours"
-            phone="(816) 842-1199"
-            description1={`"For 40 years, reStart has provided Kansas City’s most vulnerable population with a continuum of housing services. Our unique programming allows us to act as the only homeless agency in Kansas City serving all homeless populations including singles, families, couples, individuals experiencing mental health illnesses, LGBTQ+ identifying individuals, veterans, unaccompanied youth, non-traditional families and persons with HIV/AIDS.`}
-            description2={`Our organization’s culture of inclusion allows us to provide services to everyone at risk or currently experiencing homelessness. We accept people as they are and don’t require those asking for help to attest to a differing belief, religious affiliation, or traditional family structure."`}
-          />
-          <ResourceCard
-            title="Restart Inc"
-            website="https://www.restartinc.org/"
-            hours="Open 24 hours"
-            phone="(816) 842-1199"
-            email="restart@restartinc.org"
-            description1={`"For 40 years, reStart has provided Kansas City’s most vulnerable population with a continuum of housing services. Our unique programming allows us to act as the only homeless agency in Kansas City serving all homeless populations including singles, families, couples, individuals experiencing mental health illnesses, LGBTQ+ identifying individuals, veterans, unaccompanied youth, non-traditional families and persons with HIV/AIDS."`}
-          />
-          <ResourceCard
-            title="Restart Inc"
-            website="https://www.restartinc.org/"
-            hours="Open 24 hours"
-            phone="(816) 842-1199"
-            showBottomLine={false}
-          />
-        </AccordionDetails>
-      </Accordion>
-      <Accordion {...accordionProps}>
-        <AccordionSummary expandIcon={<ExpandMore />}>
-          <Typography {...accordionHeaderProps}>Food</Typography>
-        </AccordionSummary>
-        <HorizontalHeaderLine />
-        <AccordionDetails>
-          <ResourceCard
-            title="Restart Inc"
-            website="https://www.restartinc.org/"
-            description1={`"For 40 years, reStart has provided Kansas City’s most vulnerable population with a continuum of housing services. Our unique programming allows us to act as the only homeless agency in Kansas City serving all homeless populations including singles, families, couples, individuals experiencing mental health illnesses, LGBTQ+ identifying individuals, veterans, unaccompanied youth, non-traditional families and persons with HIV/AIDS."`}
-          />
-          <ResourceCard
-            title="Restart Inc"
-            website="https://www.restartinc.org/"
-            showBottomLine={false}
-          />
-        </AccordionDetails>
-      </Accordion>
-      <Accordion {...accordionProps}>
-        <AccordionSummary expandIcon={<ExpandMore />}>
-          <Typography {...accordionHeaderProps}>Clothes</Typography>
-        </AccordionSummary>
-        <HorizontalHeaderLine />
-        <AccordionDetails>
-          <ResourceCard
-            title="Restart Inc"
-            website="https://www.restartinc.org/"
-            hours="Open 24 hours"
-            phone="(816) 842-1199"
-            email="someEmail@email.com"
-          />
-          <ResourceCard
-            title="Restart Inc"
-            website="https://www.restartinc.org/"
-            hours="Open 24 hours"
-            phone="(816) 842-1199"
-            description1={`"For 40 years, reStart has provided Kansas City’s most vulnerable population with a continuum of housing services. Our unique programming allows us to act as the only homeless agency in Kansas City serving all homeless populations including singles, families, couples, individuals experiencing mental health illnesses, LGBTQ+ identifying individuals, veterans, unaccompanied youth, non-traditional families and persons with HIV/AIDS."`}
-            showBottomLine={false}
-          />
-        </AccordionDetails>
-      </Accordion>
-      <Accordion {...accordionProps}>
-        <AccordionSummary expandIcon={<ExpandMore />}>
-          <Typography {...accordionHeaderProps}>Healthcare</Typography>
-        </AccordionSummary>
-        <HorizontalHeaderLine />
-        <AccordionDetails>
-          <ResourceCard
-            title="Restart Inc"
-            website="https://www.restartinc.org/"
-            hours="Open 24 hours"
-            phone="(816) 842-1199"
-            showBottomLine={false}
-          />
-        </AccordionDetails>
-      </Accordion>
-      <Accordion {...accordionProps}>
-        <AccordionSummary expandIcon={<ExpandMore />}>
-          <Typography {...accordionHeaderProps}>Mental Health</Typography>
-        </AccordionSummary>
-        <HorizontalHeaderLine />
-        <AccordionDetails>
-          <ResourceCard
-            title="Restart Inc"
-            website="https://www.restartinc.org/"
-            hours="Open 24 hours"
-            phone="(816) 842-1199"
-            showBottomLine={false}
-          />
-        </AccordionDetails>
-      </Accordion>
-    </div>
+      <div>
+          <Typography variant="h4" component="h2" style={{ marginBottom: 25 }}>
+              Resources
+          </Typography>
+              {
+                  Object.keys(resourceDetails).map((sectionTitle: string) => {
+                      const details = resourceDetails[sectionTitle];
+                      const resourceCards = details.map((resource : any, index: number) => index === details.length - 1 ? <ResourceCard showBottomLine={false} {...resource}/> : <ResourceCard {...resource} />);
+                      return <Accordion {...accordionProps}><AccordionSummary expandIcon={<ExpandMore />}>
+                          <Typography {...accordionHeaderProps}>{sectionTitle}</Typography>
+                      </AccordionSummary>;
+                      <HorizontalHeaderLine />
+                      <AccordionDetails>{resourceCards}</AccordionDetails></Accordion>
+                  })
+              }
+      </div>
   );
 };
 
