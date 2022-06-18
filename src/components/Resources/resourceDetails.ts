d42c994c36e1dc3547094808cab5753d0f05a2e6
Jim McStanton <jim@jhmcstanton.com>
2022-06-17 19:45:18 -0500

    Removing duplicates

--- src/components/Resources/resourceDetails.ts
+++ src/components/Resources/resourceDetails.ts
@@ -8,45 +8,6 @@ Housing:
     description: |
       For 40 years, reStart has provided Kansas City’s most vulnerable population with a continuum of housing services. Our unique programming allows us to act as the only homeless agency in Kansas City serving all homeless populations including singles, families, couples, individuals experiencing mental health illnesses, LGBTQ+ identifying individuals, veterans, unaccompanied youth, non-traditional families and persons with HIV/AIDS.
       Our organization’s culture of inclusion allows us to provide services to everyone at risk or currently experiencing homelessness. We accept people as they are and don’t require those asking for help to attest to a differing belief, religious affiliation, or traditional family structure.
-  - title: "Restart Inc"
-    website: "https://www.restartinc.org/"
-    hours: "Open 24 hours"
-    phone: "(816) 842-1199"
-    description: |
-      For 40 years, reStart has provided Kansas City’s most vulnerable population with a continuum of housing services. Our unique programming allows us to act as the only homeless agency in Kansas City serving all homeless populations including singles, families, couples, individuals experiencing mental health illnesses, LGBTQ+ identifying individuals, veterans, unaccompanied youth, non-traditional families and persons with HIV/AIDS.
-      Our organization’s culture of inclusion allows us to provide services to everyone at risk or currently experiencing homelessness. We accept people as they are and don’t require those asking for help to attest to a differing belief, religious affiliation, or traditional family structure.
-Food:
-  - title: "Restart Inc"
-    website: "https://www.restartinc.org/"
-    hours: "Open 24 hours"
-    phone: "(816) 842-1199"
-    description: |
-      For 40 years, reStart has provided Kansas City’s most vulnerable population with a continuum of housing services. Our unique programming allows us to act as the only homeless agency in Kansas City serving all homeless populations including singles, families, couples, individuals experiencing mental health illnesses, LGBTQ+ identifying individuals, veterans, unaccompanied youth, non-traditional families and persons with HIV/AIDS.
-      Our organization’s culture of inclusion allows us to provide services to everyone at risk or currently experiencing homelessness. We accept people as they are and don’t require those asking for help to attest to a differing belief, religious affiliation, or traditional family structure.
-Clothes:
-  - title: "Restart Inc"
-    website: "https://www.restartinc.org/"
-    hours: "Open 24 hours"
-    phone: "(816) 842-1199"
-    description: |
-      For 40 years, reStart has provided Kansas City’s most vulnerable population with a continuum of housing services. Our unique programming allows us to act as the only homeless agency in Kansas City serving all homeless populations including singles, families, couples, individuals experiencing mental health illnesses, LGBTQ+ identifying individuals, veterans, unaccompanied youth, non-traditional families and persons with HIV/AIDS.
-      Our organization’s culture of inclusion allows us to provide services to everyone at risk or currently experiencing homelessness. We accept people as they are and don’t require those asking for help to attest to a differing belief, religious affiliation, or traditional family structure.
-Healthcare:
-  - title: "Restart Inc"
-    website: "https://www.restartinc.org/"
-    hours: "Open 24 hours"
-    phone: "(816) 842-1199"
-    description: |
-      For 40 years, reStart has provided Kansas City’s most vulnerable population with a continuum of housing services. Our unique programming allows us to act as the only homeless agency in Kansas City serving all homeless populations including singles, families, couples, individuals experiencing mental health illnesses, LGBTQ+ identifying individuals, veterans, unaccompanied youth, non-traditional families and persons with HIV/AIDS.
-      Our organization’s culture of inclusion allows us to provide services to everyone at risk or currently experiencing homelessness. We accept people as they are and don’t require those asking for help to attest to a differing belief, religious affiliation, or traditional family structure.
-"Mental Health":
-  - title: "Restart Inc"
-    website: "https://www.restartinc.org/"
-    hours: "Open 24 hours"
-    phone: "(816) 842-1199"
-    description: |
-      For 40 years, reStart has provided Kansas City’s most vulnerable population with a continuum of housing services. Our unique programming allows us to act as the only homeless agency in Kansas City serving all homeless populations including singles, families, couples, individuals experiencing mental health illnesses, LGBTQ+ identifying individuals, veterans, unaccompanied youth, non-traditional families and persons with HIV/AIDS.
-      Our organization’s culture of inclusion allows us to provide services to everyone at risk or currently experiencing homelessness. We accept people as they are and don’t require those asking for help to attest to a differing belief, religious affiliation, or traditional family structure.
 `;
 
 export default resourceDetails;
