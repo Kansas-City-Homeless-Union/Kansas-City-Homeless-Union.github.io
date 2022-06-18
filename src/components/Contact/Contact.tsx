b5e1130b48c4a994313a339779e4ab1e5fed7518
Jim McStanton <jim@jhmcstanton.com>
2022-06-17 21:49:53 -0500

    Added formspree form

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -16,56 +16,56 @@ const Contact = () => {
   };
 
   return (
-    <div>
       <div>
-        <form name="contact" method="post">
-          <input type="hidden" name="form-name" value="contact" />
-          <header>
-            <Typography variant="h4" style={{ marginBottom: 25 }}>
-              Contact Us
-            </Typography>
-          </header>
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              gap: 15,
-              width: "80%",
-              margin: "auto",
-            }}
-          >
-            <TextField
-              label="Return Email"
-              placeholder="Your Email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-            />
-            <TextField
-              label="Title"
-              placeholder="Subject"
-              name="title"
-              value={formData.title}
-              onChange={handleChange}
-              required
-            />
-            <TextField
-              name="message"
-              label="Message"
-              placeholder="Your message here..."
-              value={formData.message}
-              onChange={handleChange}
-              rows={3}
-              multiline
-              required
-            />
+          <div>
+              <form name="contact" method="POST" action="https://formspree.io/f/xeqnwjzo">
+                  <input type="hidden" name="form-name" value="contact" />
+                  <header>
+                      <Typography variant="h4" style={{ marginBottom: 25 }}>
+                          Contact Us
+                      </Typography>
+                  </header>
+                  <div
+                      style={{
+                          display: "flex",
+                          flexDirection: "column",
+                          gap: 15,
+                          width: "80%",
+                          margin: "auto",
+                      }}
+                  >
+                      <TextField
+                          label="Return Email"
+                          placeholder="Your Email"
+                          name="email"
+                          value={formData.email}
+                          onChange={handleChange}
+                      />
+                      <TextField
+                          label="Title"
+                          placeholder="Subject"
+                          name="title"
+                          value={formData.title}
+                          onChange={handleChange}
+                          required
+                      />
+                      <TextField
+                          name="message"
+                          label="Message"
+                          placeholder="Your message here..."
+                          value={formData.message}
+                          onChange={handleChange}
+                          rows={3}
+                          multiline
+                          required
+                      />
+                  </div>
+                  <Button type="submit" variant="contained" style={{ marginTop: 20 }}>
+                      Submit
+                  </Button>
+              </form>
           </div>
-          <Button type="submit" variant="contained" style={{ marginTop: 20 }}>
-            Submit
-          </Button>
-        </form>
       </div>
-    </div>
   );
 };
 
