e8d12f9e307feaeda37abf0a758e8b965f9a4ce6
Jim McStanton <jim@jhmcstanton.com>
2022-06-22 11:51:38 -0500

    Rename + cp to mv

---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+git stash
+npm run build
+set +e # Fails the script if gh-pages does not exist
+git branch -D gh-pages
+set -e
+git switch -c gh-pages
+
+mv  build/* .
+git add .
+git commit -m 'Publish site'
+git push origin -f gh-pages:gh-pages
+git switch -
+git branch -D gh-pages
